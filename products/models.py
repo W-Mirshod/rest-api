e81dc58ff0afd4a436faeac8bcf997b52a1b05c2
Mirshod <python3526@gmail.com>
2024-08-04 12:52:38 +0500

    models created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,105 @@
+from typing import Any
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    title = models.CharField(max_length=70)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='images/category/')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Group(BaseModel):
+    title = models.CharField(max_length=90)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='images/group/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super(Group, self).save(*args, **kwargs)
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    discount = models.IntegerField(default=0)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    is_liked = models.ManyToManyField(User, related_name='liked_products', blank=True)
+
+    @property
+    def discounted_price(self) -> Any:
+        if self.discount > 0:
+            return self.price * (1 - (self.discount / 100.0))
+        return self.price
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super(Product, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class Image(BaseModel):
+    image = models.ImageField(upload_to='images/products/')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    is_primary = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product.name
+
+
+class Comment(BaseModel):
+    class Rating(models.IntegerChoices):
+        One = 1
+        Two = 2
+        Three = 3
+        Four = 4
+        Five = 5
+
+    message = models.TextField()
+    rating = models.IntegerField(choices=Rating.choices, default=Rating.One.value)
+    file = models.FileField(upload_to='comments/')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Key(BaseModel):
+    name = models.CharField(max_length=70)
+
+
+class Value(BaseModel):
+    name = models.CharField(max_length=250)
+
+
+class Attribute(models.Model):
+    key = models.ForeignKey(Key, on_delete=models.CASCADE)
+    value = models.ForeignKey(Value, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- root/settings.py
+++ root/settings.py
@@ -118,7 +118,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
