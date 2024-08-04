8c01a576c2f7d4cb86bddfb70f27305e27229eee
Mirshod <python3526@gmail.com>
2024-08-04 13:26:59 +0500

    Rest API framework installed

--- root/settings.py
+++ root/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
     'social_django',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,11 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
